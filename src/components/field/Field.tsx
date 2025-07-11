0d821f0079e599e442f93b17ebaebec163e2e99a
Taner Çelikkıran <tanercelikkiran@gmail.com>
2025-07-11 10:14:35 +0300

    update Field component to manage internal state and handle clicks for state transitions

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -1,15 +1,38 @@
 "use client";
+import { useState } from "react";
 import styles from "./Field.module.css";
 
 interface FieldProps {
-    state?: string;
-    onClick?: () => void;
+  state?: string;
 }
 
-export default function Field({ state, onClick }: FieldProps) {
+export default function Field() {
+  const [state, setState] = useState("Ready");
+  function handleClick() {
+    switch (state) {
+      case "Ready":
+        setState("Seed");
+        break;
+      case "Seed":
+        setState("Sapling");
+        break;
+      case "Sapling":
+        setState("Plant");
+        break;
+      case "Plant":
+        setState("Flower");
+        break;
+      case "Flower":
+        setState("Dried Flower");
+        break;
+    }
+  }
+
   return (
-    <div className={styles.field} onClick={onClick}>
-      <label htmlFor="">{state}</label>
+    <div>
+      <button className={styles.field} onClick={handleClick}>
+        {state}
+      </button>
     </div>
   );
 }
